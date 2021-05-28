38b6d3b6a17238910b4ec082a29f47efc8dfb2ce
BlancaML <blancamorenodeluna@gmail.com>
2021-05-28 19:59:45 +0200

    home rendered

--- config/routes.config.js
+++ config/routes.config.js
@@ -13,10 +13,13 @@ const router = express.Router();
 
 // Iteration 1: link GET '/' with common controller home
 
-router.get('/');
+router.get('/', common.home);
 
 
 // Iteration 3: link GET '/tweets' with tweets controller list
+
+router.get('/', tweets.list);
+
 // Iteration 4: link POST '/tweets' with tweets controller create
 // Iteration 5: link POST '/tweets/:id/delete' with tweets controller delete
 
--- controllers/common.controller.js
+++ controllers/common.controller.js
@@ -1,5 +1,5 @@
 
 module.exports.home = (req, res, next) => {
-  res.render('/tweets/home')
+  res.render('tweets/home')
   // Iteration 1: render home view
 }
