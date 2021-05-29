9e3db2abb5b24e0fa8eb355e6dfaf47da0f437dd
BlancaML <blancamorenodeluna@gmail.com>
2021-05-29 21:34:41 +0200

    iteration 4

--- controllers/tweets.controller.js
+++ controllers/tweets.controller.js
@@ -3,18 +3,25 @@ const dataTweets = require('../data/tweets')
 
 // Iteration 3: list tweets from file
 module.exports.list = (req, res, next) => {
-  dataTweets.sort((tweet1, tweet2) => {
-    tweet2.createdAt - tweet1.createdAt
-  
-  });
-  
-  res.render('tweets/list', {tweets: dataTweets})
+
+// Iteration 4: filter tweets by user checking the query param 'name'
+const user = req.query.user;
+let tweets;
+if (user) {
+  tweets = dataTweets.filter((tweet) => tweet.user.toLowerCase().includes(user))
+} else {
+  tweets = dataTweets;
+}
+
+// Order tweets desc by date
+  dataTweets.sort((a, b) => a.createdAt - b.createdAt);
+  res.render('tweets/list', {tweets})
   
 }
 
 
-  // Order tweets desc by date
-  // Iteration 4: filter tweets by user checking the query param 'name'
+  
+  
 
 
 // Iteration 5: Create tweet validating body params
